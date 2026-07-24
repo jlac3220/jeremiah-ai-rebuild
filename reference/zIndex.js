4d0ade3831713ebb7860187e71a515df451e9c8b
jlace3320 <jlace3320@gmail.com>
2026-07-24 11:39:59 -0500

    Delete reference directory

--- reference/zIndex.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://jogycozrhylfzpwcoeed.supabase.co"; 
-const supabaseAnonKey = "sb_publishable_aGrwoKPUYvRetC4XF2mwMg_KGiGz3qe";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
